7a82f3b287d387c710f6dd0df3fab730cd1436f0
liuyx <lyx_syn@163.com>
2020-04-27 10:45:10 +0800

    modify

--- src/main/java/learn/springboot/helloworld/Application.java
+++ src/main/java/learn/springboot/helloworld/Application.java
@@ -17,6 +17,8 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        System.out.println("hahhahha");
         SpringApplication.run(Application.class, args);
+        System.out.println("hahhahha");
     }
 }
